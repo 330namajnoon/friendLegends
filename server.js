fa401a6ce09b46894191e9906dd979ee10b30dad
330namajnoon <sina.cappac@gmail.com>
2023-09-15 02:35:40 +0200

    s

--- server.js
+++ server.js
@@ -6,6 +6,23 @@ const app = express();
 const pdp = path.join(__dirname,"./");
 app.use(express.static(pdp));
 const server = http.createServer(app);
+const {Server} = require("socket.io");
+const io = new Server(server);
+
 server.listen(port,()=> {
     console.log(`server is up on port ${port}! `);
+})
+
+
+io.on("connection",(client)=> {
+    console.log("new connect");
+    
+    client.on("anim",(sina)=> {
+        
+        io.emit("anim",sina);
+    })
+
+    client.on("disconnect",()=> {
+        console.log(`new desconnect ${client.id}`);
+    })
 })
--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,22 @@ import Sina from "./Sina.js";
 import imageTest from "./imageTest.js";
 
 imageTest();
-
+const socket = io();
 const pelota1 = new Pelota(100,100,50);
 //const sina = new Sina(100,100,1,1,60,96);
 const sina1 = new Sina(200,910,1,5,70,110);
 
 canvas.addNewObject([sina1]);
+socket.on("anim",(sina)=> {
+    console.log("hola");
+    sina1.x = sina.x;
+    sina1.y = sina.y;
+    canvas.anim();
+    if(localStorage.getItem("player") == "sina")
+        socket.emit("anim",sina1);
+})
+
+if(localStorage.getItem("player") == "sina")
+    socket.emit("anim",sina1);
+
 canvas.anim();
